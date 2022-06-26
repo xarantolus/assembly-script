75c4b0518c172884924895c8cbc7589d32389566
xarantolus <xarantolus@protonmail.com>
2022-06-26 17:52:28 +0200

    More encoding steps

--- src/encoder/encoder.rs
+++ src/encoder/encoder.rs
@@ -18,7 +18,20 @@ fn iced_err_to_string(e: IcedError) -> String {
     return format!("{}", e).to_string();
 }
 
-pub fn encode_file(input: InputFile, instr_start_address: u64) -> Result<Vec<u8>, String> {
+#[derive(Debug, Clone)]
+pub struct EncodeResult {
+    code: Vec<u8>,
+    first_instr_address: u64,
+
+    data_start_address: u64,
+    data_section_size: u64,
+}
+
+pub fn encode_file(
+    input: InputFile,
+    instr_start_address: u64,
+    data_start_address: u64,
+) -> Result<EncodeResult, String> {
     let instructions: &mut Vec<Instruction> = &mut vec![];
 
     // Maps a label name to an instruction index
@@ -27,6 +40,38 @@ pub fn encode_file(input: InputFile, instr_start_address: u64) -> Result<Vec<u8>
     let mut add = |i: Result<Instruction, IcedError>| -> Result<(), String> {
         instructions.push(i.map_err(iced_err_to_string)?);
         Ok(())
+    };
+
+    // data_section maps a data label name to its offset and data size
+    let mut data_section: Vec<(String, i64, i8)> = vec![];
+    let mut next_data_section_offset = 0;
+
+    let mut labeled_mem_operand = |label: String, size: i8| -> Result<MemoryOperand, String> {
+        match data_section.iter().find(|(l, _, _)| label.eq(l)) {
+            Some((_, offset, s)) => {
+                if size.eq(s) {
+                    Ok(MemoryOperand::with_displ(
+                        data_start_address + offset.clone() as u64,
+                        64,
+                    ))
+                } else {
+                    Err(
+                        format!("data label {} has size {} but expected {}", label, s, size)
+                            .to_string(),
+                    )
+                }
+            }
+            None => {
+                let next = MemoryOperand::with_displ(
+                    data_start_address + next_data_section_offset as u64,
+                    64,
+                );
+                data_section.push((label, next_data_section_offset, size));
+                next_data_section_offset += i64::from(size);
+
+                Ok(next)
+            }
+        }
     };
 
     for (idx, line) in input.parsed_lines.iter().enumerate() {
@@ -59,10 +104,102 @@ pub fn encode_file(input: InputFile, instr_start_address: u64) -> Result<Vec<u8>
                             i.clone(),
                         ))?;
                     }
+                    (ValueOperand::Memory { label, size }, ValueOperand::Immediate { i }) => {
+                        match size.clone() {
+                            1 => add(Instruction::with2(
+                                Code::Mov_rm8_imm8,
+                                labeled_mem_operand(label.clone(), size.clone())?,
+                                *i as i32,
+                            ))?,
+                            2 => add(Instruction::with2(
+                                Code::Mov_rm16_imm16,
+                                labeled_mem_operand(label.clone(), size.clone())?,
+                                *i as i32,
+                            ))?,
+                            4 => add(Instruction::with2(
+                                Code::Mov_rm32_imm32,
+                                labeled_mem_operand(label.clone(), size.clone())?,
+                                *i as i32,
+                            ))?,
+                            _ => {
+                                return Err(format!(
+                                    "Unsupported immediate size {} for MOV",
+                                    size.clone()
+                                )
+                                .to_string());
+                            }
+                        }
+                    }
+                    (ValueOperand::Memory { label, size }, ValueOperand::Register { r }) => {
+                        match r.size {
+                            1 => {
+                                add(Instruction::with2(
+                                    Code::Mov_rm8_r8,
+                                    labeled_mem_operand(label.clone(), size.clone())?,
+                                    REGISTERS.get(&r.name.as_str()).unwrap().clone(),
+                                ))?;
+                            }
+                            2 => {
+                                add(Instruction::with2(
+                                    Code::Mov_rm16_r16,
+                                    labeled_mem_operand(label.clone(), size.clone())?,
+                                    REGISTERS.get(&r.name.as_str()).unwrap().clone(),
+                                ))?;
+                            }
+                            4 => {
+                                add(Instruction::with2(
+                                    Code::Mov_rm32_r32,
+                                    labeled_mem_operand(label.clone(), size.clone())?,
+                                    REGISTERS.get(&r.name.as_str()).unwrap().clone(),
+                                ))?;
+                            }
+                            8 => add(Instruction::with2(
+                                Code::Mov_rm64_r64,
+                                labeled_mem_operand(label.clone(), size.clone())?,
+                                REGISTERS.get(&r.name.as_str()).unwrap().clone(),
+                            ))?,
+                            _ => {
+                                return Err(format!(
+                                    "Unsupported register size {} for MOV",
+                                    r.size
+                                )
+                                .to_string());
+                            }
+                        };
+                    }
                     _ => {
-                        return Err("invalid combination of operands for mov".to_string());
+                        return Err(format!(
+                            "invalid combination of operands for mov: {:?} and {:?}",
+                            destination, source
+                        )
+                        .to_string());
                     }
                 },
+                // Test instruction for r64, imm64
+                instructions::Instruction::TEST { src1, src2 } => match (src1, src2) {
+                    (dst, ValueOperand::Register { r: src }) => {
+                        add(Instruction::with2(
+                            Code::Test_rm64_r64,
+                            REGISTERS.get(&dst.name.as_str()).unwrap().clone(),
+                            REGISTERS.get(&src.name.as_str()).unwrap().clone(),
+                        ))?;
+                    }
+                    (dst, ValueOperand::Immediate { i }) => {
+                        add(Instruction::with2(
+                            Code::Test_rm64_imm32,
+                            REGISTERS.get(&dst.name.as_str()).unwrap().clone(),
+                            i.clone(),
+                        ))?;
+                    }
+                    _ => {
+                        return Err(format!(
+                            "invalid combination of operands for test: {:?} and {:?}",
+                            src1, src2
+                        )
+                        .to_string());
+                    }
+                },
+
                 // TODO: Figure out how to reference labels, so they only get turned into addresses at the end
                 // instructions::Instruction::CALLlabel { label } => {
                 //     Instruction::with1(Code::Call, target)
@@ -85,14 +222,23 @@ pub fn encode_file(input: InputFile, instr_start_address: u64) -> Result<Vec<u8>
     let result =
         BlockEncoder::encode(64, block, BlockEncoderOptions::NONE).map_err(iced_err_to_string)?;
 
-    return Ok(result.code_buffer);
+    return Ok(EncodeResult {
+        code: result.code_buffer,
+        first_instr_address: instr_start_address,
+        data_start_address,
+        data_section_size: 0,
+    });
 }
 
 #[cfg(test)]
 mod test_encoder {
     use crate::{
         encoder::encoder::encode_file,
-        parser::{input, instructions::Instruction},
+        parser::{
+            input,
+            instructions::{Instruction, ValueOperand},
+            registers,
+        },
     };
 
     fn assert_encoding(i: Vec<Instruction>, u: Vec<u8>) {
@@ -105,11 +251,12 @@ mod test_encoder {
             crate::parser::input::InputFile {
                 parsed_lines: input_lines,
             },
+            0x2000,
             0x1000,
         );
         assert!(encode_res.is_ok(), "{}", encode_res.err().unwrap());
 
-        assert_eq!(encode_res.unwrap(), u);
+        assert_eq!(encode_res.unwrap().code, u);
     }
 
     #[test]
@@ -123,6 +270,35 @@ mod test_encoder {
     }
 
     #[test]
+    fn test() {
+        assert_encoding(
+            vec![Instruction::TEST {
+                src1: registers::Register {
+                    name: "RSP".to_string(),
+                    size: 8,
+                    part_of: registers::GPRegister::RSP,
+                },
+                src2: ValueOperand::Immediate { i: 0xf },
+            }],
+            vec![0x48, 0xf7, 0xc4, 0x0f, 00, 00, 00],
+        )
+    }
+
+    #[test]
+    fn mov_mem_imm8() {
+        assert_encoding(
+            vec![Instruction::MOV {
+                destination: ValueOperand::Memory {
+                    label: "foo".to_string(),
+                    size: 1,
+                },
+                source: ValueOperand::Immediate { i: 0x42 },
+            }],
+            vec![0xc6, 0x4, 0x25, 0, 16, 0, 0, 66],
+        );
+    }
+
+    #[test]
     fn alphabet() {
         let alphabet_assembly = include_str!(concat!(
             env!("CARGO_MANIFEST_DIR"),
@@ -131,7 +307,7 @@ mod test_encoder {
         let result = input::parse_gnu_as_input(alphabet_assembly.to_string());
         assert!(result.is_ok(), "{}", result.err().unwrap());
 
-        let encode_res = encode_file(result.unwrap(), 0x1000);
+        let encode_res = encode_file(result.unwrap(), 0x2000, 0x1000);
         assert!(encode_res.is_ok(), "{}", encode_res.err().unwrap());
     }
 }
